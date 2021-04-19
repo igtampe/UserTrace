4040365b147949c7f26a55c81fa810425fb791e6
Ignacio Tampe <Igtampe@outlook.com>
2021-04-19 00:41:11 -0400

    Cleanup

--- Igtampe.UserTracer/Properties/Settings.Designer.cs
+++ Igtampe.UserTracer/Properties/Settings.Designer.cs
@@ -22,14 +22,5 @@ namespace Igtampe.UserTracer.Properties {
                 return defaultInstance;
             }
         }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string Colors {
-            get {
-                return ((string)(this["Colors"]));
-            }
-        }
     }
 }
