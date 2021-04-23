fb16290c02c909aa59db22cc3fb7af9a33fbf8f0
Ignacio Tampe <Igtampe@outlook.com>
2021-04-23 18:55:25 -0400

    New About Form

--- Igtampe.UserTracer/Forms/AboutForm.Designer.cs
+++ Igtampe.UserTracer/Forms/AboutForm.Designer.cs
@@ -32,7 +32,7 @@ namespace Igtampe.UserTracer {
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(660, 233);
+            this.button1.Location = new System.Drawing.Point(195, 157);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -43,30 +43,31 @@ namespace Igtampe.UserTracer {
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Igtampe.UserTracer.Properties.Resources.Banner;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(12, -47);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(723, 215);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.Size = new System.Drawing.Size(424, 215);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(12, 230);
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(82, 103);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(723, 26);
+            this.label1.Size = new System.Drawing.Size(286, 51);
             this.label1.TabIndex = 2;
-            this.label1.Text = "UserTrace (Version 1.0)\r\n(C)2021 Igtampe, No Rice (or Rights) Reserved\r\n";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Text = "UserTrace (Version 2.0)\r\n(C)2021 Igtampe, No Rice (or Rights) Reserved\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // AboutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(744, 268);
+            this.ClientSize = new System.Drawing.Size(452, 192);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "AboutForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
